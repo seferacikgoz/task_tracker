6451472065bfb1d37cba120de731a89b2f59adbf
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-30 01:10:52 +0300

    show & hide

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
 import Button from "react-bootstrap/Button"
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [text, setText] = useState("Show Task Bar")
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+    const buttonText = isOpen ? "Show Task Bar" : "Hide Task Bar"
+    setText(buttonText);
+  }
   return (
     <div>
 
-      <Button variant="primary">Show Task Bar</Button>
-      <AddTask/>   
+      <Button 
+      onClick={() => {toggle()}}
+      variant="danger">{text}</Button>
+
+      {isOpen && <AddTask/> }
+        
       <TaskList/>
     </div>
   );
