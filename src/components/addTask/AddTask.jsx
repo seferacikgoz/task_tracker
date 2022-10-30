d91776eaca61c50516d4fb9a0bee916599c5193a
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-30 11:19:43 +0300

    onChange

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 const AddTask = () => {
+
+  const [task, setTask] = useState("")
+  const [date, setDate] = useState("")
   return (
     <div> 
     <Form>
     <Form.Group className="mb-3" controlId="formBasicEmail">
       <Form.Label>Task</Form.Label>
-      <Form.Control type="text" placeholder="Enter task" />
+      <Form.Control type="text" 
+      placeholder="Enter task"
+      onChange={(e) => setTask(e.target.value)} />
     </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicPassword">
       <Form.Label>Date</Form.Label>
-      <Form.Control type="date"/>
+      <Form.Control type="date"
+      onChange={(e)=> setDate(e.target.value)}/>
     </Form.Group>
-    <Button variant="primary" type="submit">
+    <Button variant="primary w-50" type="submit">
       SAVE
     </Button>
   </Form></div>
