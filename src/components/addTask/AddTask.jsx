7cab144cc8d111e30e719cc9a9dc95b5d0a40ae8
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-30 11:06:01 +0300

    addTask

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <div> 
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Task</Form.Label>
+      <Form.Control type="text" placeholder="Enter task" />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Date</Form.Label>
+      <Form.Control type="date"/>
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      SAVE
+    </Button>
+  </Form></div>
   )
 }
 
